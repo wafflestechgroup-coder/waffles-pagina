0c6e059123a070372628dea97df9e42bec37ffce
wafflestechgroup-coder <waffles.tech.group@gmail.com>
2025-09-13 08:22:25 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+// Aquí puedes agregar interactividad futurista después.
+// Ejemplo: alerta divertida al cargar
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("⚡ WAFFLES TECH está vivo. Ya existimos.");
+});
